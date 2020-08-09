4e8296f61390e4564a80353798f64171a0d61907
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-09 22:07:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = []
-  array.push("singing")
+  hobbies << "singing"
+  p hobbies
 end
 
 def add_element_to_start_of_array(array, element)
